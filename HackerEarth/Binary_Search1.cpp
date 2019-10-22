f9e6b5c737865f38b700e6f8f2a2bce1cf9e81a3
Akanksha Garg <48736568+akugarg@users.noreply.github.com>
2019-10-22 20:01:48 +0530

    Create Binary_Search1.cpp

---
+++ HackerEarth/Binary_Search1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+//https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/picu-bank-09e29493/editorial/
+int main() {
+	// your code goes here
+	ll d,a,m,b,x;
+	cin>>d>>a>>m>>b>>x;
+	ll amt=x-d;
+	int days=0;
+	int s=amt/(a*m+b);
+	days+=s*(m+1);
+    int l=amt%(a*m+b);
+    if(l<=a*m-b)
+    {
+        days+=ceil((double)l/m);
+    }
+    else
+    days+=m+1;
+	cout<<days;
+	return 0;
+}
